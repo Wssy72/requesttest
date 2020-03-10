a8f3d1018e434d9da3ffb1f05bb26539011d9ba2
Sergey <dir@syakovlev.ru>
2020-03-10 17:43:49 +0500

    request works

--- requestTest/ViewController.swift
+++ requestTest/ViewController.swift
@@ -17,27 +17,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         oneRequest()
     }
-
-func oneRequest () {
-let url = URL(string: "https://reqres.in/api/users?page=2")!
-//var request = URLRequest(url: url)
-let session = URLSession.shared
-
-let task = session.dataTask(with: url) {(data, response, error) in
-if let response = response {
-               print(response)
-           }
-           
-           if let data = data {
-               print(data)
-            do {
-                let json = try JSONSerialization.jsonObject(with: data, options: [])
-                print(json)
-            } catch {
-                print(error)
-            }
-}  
     
-}
-}
+    func oneRequest () {
+        let url = URL(string: "https://reqres.in/api/users?page=2")!
+        //var request = URLRequest(url: url)
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: url) {(data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                print(data)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+
+        }
+    .resume()
+    }
 }
