e9ea19980b63931be42a1ef727f377888540f3a7
Sergey <dir@syakovlev.ru>
2020-03-31 16:56:18 +0500

    fix bag load data
    change request with funcs UITableView

--- requestTest/ViewController.swift
+++ requestTest/ViewController.swift
@@ -18,15 +18,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
         oneRequest()
+        
         myTableView = UITableView.init(frame: view.bounds, style: UITableView.Style.grouped)
-        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "registerCell")
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: registerCell)
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        myTableView.frame = CGRect(x: 0, y: 0, width: displayWidth, height: displayHeight)
         view.backgroundColor = .white
+        
         self.myTableView.dataSource = self
         self.myTableView.delegate = self
         view.addSubview(myTableView)
-        self.myTableView.reloadData()
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataDecode.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: registerCell, for: indexPath)
+        let userNumber = indexPath.row
+        let user = dataDecode[userNumber]
+        //print("\(dataDecode[userNumber])")
+        cell.textLabel?.text = dataDecode[userNumber].avatar // user.firstName
+        return cell
+    }
+    
     func oneRequest () {
         let url = URL(string: "https://reqres.in/api/users?page=2")!
         let session = URLSession.shared
@@ -41,27 +61,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     let answerData = answer.data
                     self.dataDecode.append(contentsOf: answerData)
                     
-                   print(answer)
+                    print(answer)
                 } catch {
                     print(error)
                 }
             }
         }
-    .resume()
+        .resume()
+        print("request completed")
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return dataDecode.count
-       }
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       let cell = tableView.dequeueReusableCell(withIdentifier: registerCell, for: indexPath)
-       let userNumber = indexPath.row
-       let user = dataDecode[userNumber]
-       cell.textLabel?.text = user.firstName
-       return cell
-       }
+    
 }
 
 struct ServerAnswer: Decodable {
-let data: [User]
+    let data: [User]
 }
 
